4ca2967ddb9b875a120c6277c154224d0772a3a9
Alex-tzh <alex.tzh1997@gmail.com>
2024-03-27 16:13:58 +0800

    Pipeline regressor update

--- notebooks/test_pipe.py
+++ notebooks/test_pipe.py
@@ -3,16 +3,13 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.linear_model import LinearRegression, Lasso, LassoCV, Ridge, RidgeCV, ElasticNet, ElasticNetCV
-
-
 class DataPipeLine :
 
     def __init__(self, data_source):
         self.data_path = data_source
         self.data = None
         self.scaled_data = None
-        self.regressor.data = None
-        self.number = 0.4
+        self.regressor_data = None
 
     def load_data(self):
         self.data = pd.read_csv(self.data_path)
@@ -24,12 +21,10 @@ class DataPipeLine :
         data_clean['SCRAPE_DATE'] = pd.to_datetime(self.data['SCRAPE_DATE'])
         df_clean['MANUFACTURED_YEAR'] = df_clean['MANUFACTURED_YEAR'].astype(int)
 
-
         df_clean=dropped_data
         df_main['SCRAPE_DATE'] = pd.to_datetime(df_main['SCRAPE_DATE'])
         df_clean['MANUFACTURED_YEAR'] = df_clean['MANUFACTURED_YEAR'].astype(int)
         df_clean['CAR_AGE'] = df_main['SCRAPE_DATE'].dt.year  - df_clean['MANUFACTURED_YEAR'] # Obtaining values for age of car
-
 
     def scale_data(self):
         scaler = StandardScaler()
@@ -42,5 +37,6 @@ class DataPipeLine :
     def run_pipeline(self, n_components):
         self.load_data()
         self.scale_data()
+        self.regression()
         self.perform_pca(n_components)
 
