a9f8f9f94e18c6b34e51d0dddc338843f6e2103a
Alex-tzh <alex.tzh1997@gmail.com>
2024-03-27 15:31:37 +0800

    Update Pipeline

--- notebooks/test_pipe.py
+++ notebooks/test_pipe.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+import numpy as np
+from sklearn.linear_model import LinearRegression, Lasso, LassoCV, Ridge, RidgeCV, ElasticNet, ElasticNetCV
+
 
 class DataPipeLine :
 
@@ -8,18 +10,31 @@ class DataPipeLine :
         self.data_path = data_source
         self.data = None
         self.scaled_data = None
-        self.pca_data = None
+        self.number = 0.4
 
     def load_data(self):
         self.data = pd.read_csv(self.data_path)
+        return(self.number)
+
+    def clean_data(self):
+        data_clean = self.data.drop(['LISTING_URL', 'SCRAPE_DATE'],axis=1)
+        data_clean.dropna()
+        data_clean['SCRAPE_DATE'] = pd.to_datetime(self.data['SCRAPE_DATE'])
+        df_clean['MANUFACTURED_YEAR'] = df_clean['MANUFACTURED_YEAR'].astype(int)
+
+
+        df_clean=dropped_data
+        df_main['SCRAPE_DATE'] = pd.to_datetime(df_main['SCRAPE_DATE'])
+        df_clean['MANUFACTURED_YEAR'] = df_clean['MANUFACTURED_YEAR'].astype(int)
+        df_clean['CAR_AGE'] = df_main['SCRAPE_DATE'].dt.year  - df_clean['MANUFACTURED_YEAR'] # Obtaining values for age of car
+
+
+
+
 
     def scale_data(self):
         scaler = StandardScaler()
         self.scaled_data = scaler.fit_transform(self.data)
-
-    def perform_pca(self, n_components):
-        pca = PCA(n_components=n_components)
-        self.pca_data = pca.fit_transform(self.scaled_data)
 
     def run_pipeline(self, n_components):
         self.load_data()
---
+++ notebooks/testing.py
@@ -0,0 +1,6 @@
+from test_pipe import DataPipeLine
+
+pipeline = DataPipeLine('sgcarmart_used_cars_prices7.csv')
+
+
+print(pipeline.number)
