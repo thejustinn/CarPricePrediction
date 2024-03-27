6d2f20292dd4b04b2abb8e2d3b57e5c99915494b
thejustinn <79741337+thejustinn@users.noreply.github.com>
2024-03-27 19:09:09 +0800

    change pipeline file path

--- notebooks/testing.py
+++ notebooks/testing.py
@@ -1,6 +1,6 @@
 from test_pipe import DataPipeLine
 
-pipeline = DataPipeLine('sgcarmart_used_cars_prices7.csv')
+pipeline = DataPipeLine('CarPricePrediction/sgcarmart_used_cars_prices7.csv')
 
 
 print(pipeline.run_pipeline())
