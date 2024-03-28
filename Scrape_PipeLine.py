0f445bd74237b54d4a49c24d0f5363fbe946e25d
thejustinn <79741337+thejustinn@users.noreply.github.com>
2024-03-29 02:09:29 +0800

    update

--- Scrape_PipeLine.py
+++ Scrape_PipeLine.py
@@ -14,7 +14,7 @@ headers = {
     'sec-ch-ua': "\" Not A;Brand\";v=\"123\", \"Chromium\";v=\"123\", \"Google Chrome\";v=\"123\"",
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': "macOS",
-    'Upgrade-Insecure-Requests': 1,
+    'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     }
 
