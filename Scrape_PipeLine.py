89d708f13e83d24cd74ef19d39e7c3a58225df1e
thejustinn <79741337+thejustinn@users.noreply.github.com>
2024-03-29 16:31:12 +0800

    remove print in run_pipeline

--- Scrape_PipeLine.py
+++ Scrape_PipeLine.py
@@ -155,7 +155,6 @@ class Scrape_PipeLine:
 
     def run_pipeline(self):
         main_page_url = self.fetch_main_pages()
-        print(main_page_url)
         listing_url = self.fetch_listing_urls(main_page_url)
         if listing_url:
             data = self.fetch_data(listing_url)
