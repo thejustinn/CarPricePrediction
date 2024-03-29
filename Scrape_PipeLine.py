cf834af647489857a4d99e3d815d6aa4eb51da90
chingxwen <huangchingwen3@gmail.com>
2024-03-29 14:05:35 +0800

    chore : Update Scrape_Pipeline.py

--- Scrape_PipeLine.py
+++ Scrape_PipeLine.py
@@ -15,38 +15,40 @@ class Scrape_PipeLine:
                                          'SCRAPE_DATE', 'OMV', 'ARF', 'COE_FROM_SCRAPE_DATE',
                                          'DAYS_OF_COE_LEFT', 'ENGINE_CAPACITY_CC', 'CURB_WEIGHT_KG',
                                          'NO_OF_OWNERS', 'VEHICLE_TYPE', 'POST_DATE'])
-        self.filename = 'sgcarmart_used_cars_prices7'
+        self.filename = 'sgcarmart_used_cars_prices_test_one_link'
         self.base_url = 'https://www.sgcarmart.com/used_cars/'
+        
 
-    def fetch_main_page(self):
-        url = "https://www.sgcarmart.com/used_cars/listing.php?BRSR=0&RPG=100&AVL=2&VEH=2" 
-        return url
+    def fetch_main_pages(self):
+        main_page_listing_list = []
+        for idx in range(166):
+            url = "https://www.sgcarmart.com/used_cars/listing.php?BRSR=" + str(idx * 100) + "&RPG=100&AVL=2&VEH=2"
+            main_page_listing_list.append(url)
+        return main_page_listing_list
 
-    def fetch_listing_url(self, main_page_url):
-        content = requests.get(main_page_url)
-        soup = BeautifulSoup(content.text, 'lxml')
-        links = soup.find_all('a')
-        for link in links:
-            suffix = link.get('href')
-            if ('ID=' in suffix) and ('DL=' in suffix):
-                listing_url = self.base_url + suffix
-                return listing_url  
-        return None
+    def fetch_listing_urls(self, main_page_listing_list):
+        listing_urls = []
+        for main_link in main_page_listing_list:
+            content = requests.get(main_link)
+            soup = BeautifulSoup(content.text, 'lxml')
+            links = soup.find_all('a')
+            for link in links:
+                suffix = link.get('href')
+                if ('ID=' in suffix) and ('DL=' in suffix):
+                    listing_url = self.base_url + suffix
+                    listing_urls.append(listing_url)
+            time.sleep(1)
+        return list(set(listing_urls))
 
-    def brand_retrieval(self, parsed_url):
-        try:
-            return parsed_url.find(class_='nounderline globaltitle').text.split()[0]
-        except:
-            return np.nan
 
     def fetch_data(self, listing_url):
         response = requests.get(listing_url)
-        listing_url = BeautifulSoup(response.text, 'lxml')
+        # listing_url = BeautifulSoup(response.text, 'lxml')
         data = {}
         data['LISTING_URL'] = listing_url
         data['SCRAPE_DATE'] = datetime.now().strftime("%d/%m/%Y")
         try:
-            data['BRAND'] = self.brand_retrieval(listing_url)
+            data['BRAND'] = brand_retrieval(listing_url)
         except:
             data['BRAND'] = np.nan
         try:
@@ -137,6 +139,9 @@ class Scrape_PipeLine:
         
         except:
             data['POST_DATE'] = np.nan
+            
+        print(data['VEHICLE_TYPE'])
+        print(data)
         
         return data
 
@@ -145,7 +150,7 @@ class Scrape_PipeLine:
         listing_url = self.fetch_listing_url(main_page_url)
         if listing_url:
             data = self.fetch_data(listing_url)
-            self.df = self.df.append(data, ignore_index=True)
+            self.df = self.df._append(data, ignore_index=True)
             self.df.to_csv("{}.csv".format(self.filename))
             return self.df
         else:
